edd39857ded199f7c8a5a535ce4aeaca9514288b
Patrick <pqrosset@gmail.com>
2016-10-01 15:11:17 -0400

    added foundation-sites to the project

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require angular/angular
 //= require ui-router
 //= require_tree .
+
+$(function(){ $(document).foundation(); });
